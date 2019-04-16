2ac6bf3fde44f718f9c276b07ea69a418684b38a
Cyril Marchive <cyril.marchive@gmail.com>
2019-04-15 23:46:40 -0400

    Mise en place du header

--- src/components/app.js
+++ src/components/app.js
@@ -1,10 +1,11 @@
 import React, {Component} from 'react';
+import Header from '../containers/header';
 
 class App extends Component {
     render() {
         return (
             <div>
-                Ca marche
+                <Header />
             </div>
         );
     }
