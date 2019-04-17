4ea31d9dff923ef2cec7662420a3b06473d5f03d
Cyril Marchive <cyril.marchive@gmail.com>
2019-04-17 18:11:36 -0400

    Mise en place des tests

--- src/components/app.js
+++ src/components/app.js
@@ -4,17 +4,20 @@ import {Route, Switch} from 'react-router-dom';
 import Home from "./home";
 import Ressources from "./ressources";
 import requireAuth from '../helpers/require-authentication';
+import TodoApp from './todo-app';
 
 class App extends Component {
     render() {
         return (
             <div>
                 <Header />
-                <Switch>
-                    <Route exact path="/" component={Home} />
-                    <Route exact path="/accueil" component={Home} />
-                    <Route path="/ressources" component={requireAuth(Ressources)} />
-                </Switch>
+                <div className="container body_content">
+                    <Switch>
+                        <Route exact path="/" component={TodoApp} />
+                        <Route exact path="/accueil" component={Home} />
+                        <Route path="/ressources" component={requireAuth(Ressources)} />
+                    </Switch>
+                </div>
             </div>
         );
     }
---
+++ src/tests/header.test.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from '../containers/header';
+import {shallow, mount} from 'enzyme';
+import {setAuthentication} from "../actions";
+import AuthenticationReducer from '../reducers/reducer-action-info';
+import {SET_AUTHENTICATION} from "../actions/action-types";
+
+
+describe("Test sur Header", () => {
+   it("Render du composant connecte sans erreur", () => {
+
+   });
+});
