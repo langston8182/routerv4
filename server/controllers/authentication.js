3dc37d8d100de2959e77a0983f80d3ad8a7a811e
Cyril Marchive <cyril.marchive@gmail.com>
2019-04-18 18:47:22 -0400

    Mise en place serveur pour l'authentification

---
+++ server/controllers/authentication.js
@@ -0,0 +1,44 @@
+const User = require('../models/user');
+const lodash = require('lodash');
+const jwt = require('jwt-simple');
+const config = require('../config');
+
+function getTokenForUser(user) {
+    const now = new Date().getTime();
+    return jwt.encode({
+       sub: user.id,
+       iat: now
+    }, config.secret);
+}
+
+exports.signup = function(req, res, next) {
+    const email = req.body.email;
+    const password = req.body.password;
+
+    User.findOne({email: email}, function(err, existingUser) {
+        if (err) {
+            return next(err);
+        }
+        if (existingUser) {
+            return res.status(422).send({error: "Email utilise"});
+        }
+        if (lodash.isEmpty(email) || lodash.isEmpty(password)) {
+            return res.status(422).send({error: "Email ou mot de passe vide"});
+        } else {
+            const user = new User({
+                email: email,
+                password, password
+            });
+            user.save(function(err) {
+               if (err) {
+                   return next(err);
+               }
+               res.json({token: getTokenForUser(user)});
+            });
+        }
+    });
+};
+
+exports.signin = function(req, res, next) {
+    res.json({token: getTokenForUser(req.user)});
+};
---
+++ server/models/user.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+const Schema =  mongoose.Schema;
+const bcrypt = require('bcrypt-nodejs');
+
+const userSchema = new Schema({
+    email: {type: String, unique: true, lowerCase: true},
+    password: String
+});
+
+userSchema.pre("save", function(next) {
+   const user = this;
+   bcrypt.genSalt(10, function(err, salt) {
+       if (err) {
+           return next(err);
+       }
+       bcrypt.hash(user.password, salt, null, function(err, hash) {
+          if (err) {
+              return next(err);
+          }
+          user.password = hash;
+          next();
+       });
+    })
+});
+
+userSchema.methods.isPasswordEqualTo = function(externalPassword, done) {
+    bcrypt.compare(externalPassword, this.password, function(err, isMatch) {
+        if(err) {
+            return done(err);
+        }
+        return done(null, isMatch);
+    });
+};
+
+const UserModel = mongoose.model("user", userSchema);
+
+module.exports = UserModel;
